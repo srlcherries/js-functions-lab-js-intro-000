5a93650567a475d875f5c43b9a14f15e8a99db59
Sherry Lin <sherrylin222@gmail.com>
2018-04-22 23:29:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,4 @@ function happyHolidayTo(holiday, name) {
   return "Happy " + holiday + ", " + name + "!";
 }
 
-function holidayCountdown(holiday, days) {
-  return "It's " + days + " days until " + holiday "!";
-}
+
